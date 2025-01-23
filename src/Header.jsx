7de944d5250dab7961ffa91d034adc4110acbd1c
GDPM05 <j.p.f.a009@gmail.com>
2025-01-23 18:26:34 +0000

    some learning

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,9 @@
 
+import {useState} from "react";
+
 function Header(){
+
+    const [darkMode, turnedON] = useState(0);
 
     return (
 
@@ -10,7 +14,7 @@ function Header(){
                 <li class="headerFirst headerElements">Task List</li>
                 <li class="headerElements">Tasks list</li>
                 <li class="headerElements">Taks Log</li>
-                <li class="headerElements"><button>Dark / Light mode</button></li>
+                <li class="headerElements"><button onClick={() => turnedON((darkMode == "dk_") ? darkMode = "lt_" : darkMode = "dk_")}>Dark / Light mode</button></li>
             </ul>
         </header>
 
